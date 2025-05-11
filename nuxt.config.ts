23eb6052cae1eeaa6c5eef7c2d995ab15738e300
nala <nala@gmail.com>
2025-05-11 18:31:29 +0300

    Deploy to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  app: {
+    baseURL: '/nala/'  // 👈 важно: имя репозитория
+  },
+  router: {
+    options: {
+      base: '/nala/'
+    }
+  },
   target: 'static',
   generate: {
     fallback: true
